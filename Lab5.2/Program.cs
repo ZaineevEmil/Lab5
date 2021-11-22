33aa60a73fbdb97008762f58d5919336ecaa8eba
2299112@gmail.com <2299112@gmail.com>
2021-11-22 16:51:16 +0500

    Добавьте файлы проекта.

---
+++ Lab5.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Расчет суммы минимального и максимального чисел
+            int Max = 0, Min = 50;
+            const int n = 15;
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 50);
+                Console.WriteLine("{0} ", array[i]);
+                if (array[i] >= Max)
+                {
+                    Max = array[i];
+                }
+                if (array[i] <= Min)
+                {
+                    Min = array[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма максимального и минимального чисел равна {0}", Max + Min);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Расчет среднеарифмитического 7 чисел
+            double sum = 0;
+            Console.WriteLine("Введите семь чисел");
+            double[] array = new double[7];
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToDouble(Console.ReadLine());
+                sum = array[i] + sum;
+            }
+            Console.WriteLine("Среднеарифмитическое чисел = {0:f3}", sum / 7);
+            Console.ReadKey();
+        }
+    }
+}
